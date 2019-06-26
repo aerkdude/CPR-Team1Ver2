ac35bbe390f7e07e605fa0d7d80ca93aec5928b1
Peter <nepnepholyhatchet@gmail.com>
2019-06-26 15:51:36 +0700

    รวมงาน

--- VR-Team01/Assets/Scripts/Question.cs
+++ VR-Team01/Assets/Scripts/Question.cs
@@ -6,28 +6,42 @@ using UnityEngine.UI;
 public class Question : MonoBehaviour
 {
     public GameObject questionCanvas;
-    public bool activeCancas;
-    public static bool hitShoulder;
-    public float Timer;
+    
     public GameObject phone;
     public GameObject shoulder;
-
-    public bool canShowHint;
-    private string guess;
-    public string hint;
-    public string[] question;
-    public int questionNo;
+    public GameObject hintPanel;
+    public GameObject guidePanel;
     public Text questionText;
     public Text hintText;
     public Text guideText;
+
+    public static bool hitShoulder;
+    public bool activeCancas;
+    public float Timer;
+    public bool canShowHint;
+    public string[] question;
+    private string guess;
+    public string hint;
+    public int questionNo;
+
+    //select paper
+    public int curSelect;
+    public bool slot1;
+    public bool slot2;
+    public bool slot3;
+    public bool slot4;
+
     public InputField InputAnswer;
    // public int[] answer;
 
     //private Text answerText;
 
-    
+    // Start is called before the first frame update
     void Start()
     {
+        curSelect = 0;
+        guidePanel.SetActive(false);
+        hintPanel.SetActive(false);
         canShowHint = false;
         guideText.text = "";
         hitShoulder = false;
@@ -36,9 +50,15 @@ public class Question : MonoBehaviour
         hintText.text = "";
 
         StartCoroutine(preQuestion1());
+
+        //paper system
+        slot1 = false;
+        slot2 = false;
+        slot3 = false;
+        slot4 = false;
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
         if (GameController.gameStart)
@@ -72,11 +92,10 @@ public class Question : MonoBehaviour
         {
             ProcessText();
             InputAnswer.clearInputField();
-            //ShowHint();
             Timer = 0;
             questionCanvas.SetActive(false);
         }
-
+        /*
         if (Timer <= 10)
         {
             // string input = InputAnswer.ToString();
@@ -122,10 +141,12 @@ public class Question : MonoBehaviour
             Timer = 0;
             questionCanvas.SetActive(false);
         }
+        */
     }
     void ProcessText()
     {
-        guess = InputAnswer.text;
+        /*guess = InputAnswer.text;
+        //Debug.Log(guess);
         switch (questionNo)
         {
             case 0:
@@ -197,21 +218,22 @@ public class Question : MonoBehaviour
                 {
                     Debug.Log("Correct");
                     ResetAnswer();
-                    ReadyToCall();
+                    StartCoroutine(PrepareCall());
                 }
                 else
                 {
                     Debug.Log("Wrong");
                     ResetAnswer();
-                    ReadyToCall();
+                    StartCoroutine(PrepareCall());
                 }
                 break;
-        }
+        }*/
     }
 
     public void ShowHint()
     {
         hintText.text = "เฉลย: "+hint;
+        hintPanel.SetActive(true);
         StartCoroutine(ClearHint());
     }
     private void ResetAnswer()
@@ -220,18 +242,19 @@ public class Question : MonoBehaviour
         guess = "";
         Timer = 0;
         questionCanvas.SetActive(false);
-        canShowHint = false;
+        //canShowHint = false;
         InputAnswer.clearInputField();
     }
 
     IEnumerator ClearHint()
     {
         yield return new WaitForSeconds(3.0f);
+        hintPanel.SetActive(false);
         hintText.text = "";
     }
     IEnumerator preQuestion1()
     {
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
         questionCanvas.gameObject.SetActive(true);
         canShowHint = true;
         InputAnswer.ActivateInputField();
@@ -241,7 +264,8 @@ public class Question : MonoBehaviour
     }
     IEnumerator preQuestion2()
     {
-        yield return new WaitForSeconds(5.0f);
+        guidePanel.SetActive(false);
+        yield return new WaitForSeconds(2.0f);
         questionCanvas.gameObject.SetActive(true);
         canShowHint = true;
         InputAnswer.ActivateInputField();
@@ -253,21 +277,45 @@ public class Question : MonoBehaviour
     {
         yield return new WaitForSeconds(3.0f);
         shoulder.SetActive(true);
-        guideText.text = "ลองตบบ่าสกิดผู้ป่วย";
+        guidePanel.SetActive(true);
+        guideText.text = "P1 ตบบ่าสะกิดผู้ป่วย";
+    }
+    IEnumerator PrepareCall()
+    {
+        yield return new WaitForSeconds(3.0f);
+        ReadyToCall();
     }
     void ReadyToCall()
     {
         Debug.Log("Phpne spawn");
-        guideText.text = "กดโทรเรียกสพฉ.";
+        guidePanel.SetActive(true);
+        guideText.text = "P1 ใช้โทรศัพท์ที่พื้นโทรเรียกสพฉ.";
         phone.SetActive(true);
-        StartCoroutine(RemovePhoneText());
     }
-    IEnumerator RemovePhoneText()
+    
+    public void RemovePhoneText()
+    {
+        StartCoroutine(ReadyCpr());
+        guideText.text = "หน่วยแพทย์กำลังมา เตรียมนับเวลาถอยหลัง";
+    }
+    IEnumerator ReadyCpr()
     {
         yield return new WaitForSeconds(3.0f);
         guideText.text = "";
+        StartCoroutine(CprBegin());
     }
-
+    IEnumerator CprBegin()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GameController.gameStart = true;
+        GameController.timeStart = true;
+        guideText.text = "เริ่มนับบเวลาถอยหลัง 2 นาที";
+    }
+    IEnumerator CprContinue()
+    {
+        yield return new WaitForSeconds(3.0f);
+        guideText.text = "P1 ทำ CPR / P2 คอยตอบคำถาม";
+    }
 }
 
 public static class Extension
